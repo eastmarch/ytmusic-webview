70cede3a653102491be574a102903729ed22dcbe
eastmarch <29557084+eastmarch@users.noreply.github.com>
2025-04-06 14:17:45 -0300

    feat: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# ytmusic-webview
+# YT Music for Windows
 YT Music app that uses Edge WebView2
+
+![Screenshot](screenshot.png)
+
+## Features (Edge WebView2)
+- Lighter than Electron
+- Does not require Microsoft Edge
+- Up to date webview based on the current version of Chromium
+- Shared instance with other webview apps running (less memory)
+- Supports efficent mode for background processes (less battery)
+- Media controls on quick settings / Media shortcuts from the keyboard
+- Opens the app much faster compared to alternatives
+- Uses native scrollbars that support dark mode
+- Back button on the top-left
+
+## How to install
+- Enable "developer mode" on the Settings app
+- Allow the execution of local PowerShell scripts in the same tab
+- Download the zip file from releases
+- Extract it to a folder
+- Right-click `Install.ps1` and select Run with `PowerShell`
+
+
+## Reference
+- [Install Edge WebView2 if not available on the system](https://developer.microsoft.com/en-us/microsoft-edge/webview2/consumer/)
+- [WebView2 in WinUI 2 (UWP) apps](https://learn.microsoft.com/en-us/microsoft-edge/webview2/platforms/winui2-uwp)
---
+++ app/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace YTMusicUWP
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            var currentView = SystemNavigationManager.GetForCurrentView();
+            currentView.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            currentView.BackRequested += OnBackRequested;
+            this.InitializeComponent();
+        }
+
+        private void OnBackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (WebView2.CoreWebView2.CanGoBack)
+            {
+                WebView2.CoreWebView2.GoBack();
+            }
+        }
+    }
+}
